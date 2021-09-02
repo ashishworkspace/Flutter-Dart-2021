a424aa86cba09a2756e2cdd1cea565383a12c17c
Ashish Kumar <ashish kumar>
2021-09-02 20:38:39 +0530

    Future Async Await

---
+++ future.dart
@@ -0,0 +1,12 @@
+Future<void> main() async {
+  print(await showUserData());
+}
+
+Future<String> fetchUserData() async {
+  return await Future.delayed(Duration(seconds: 3), () => "Nice");
+}
+
+Future<String> showUserData() async {
+  String data = await fetchUserData();
+  return "This is user data: $data";
+}
